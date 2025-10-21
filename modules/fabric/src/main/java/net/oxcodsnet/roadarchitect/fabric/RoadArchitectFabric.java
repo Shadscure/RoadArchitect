91a229a0eed2d0453b62a2bb61c48adaf06347be
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2025-10-21 23:12:51 +0400

    v1.6.0 ---

--- modules/fabric/src/main/java/net/oxcodsnet/roadarchitect/fabric/RoadArchitectFabric.java
+++ modules/fabric/src/main/java/net/oxcodsnet/roadarchitect/fabric/RoadArchitectFabric.java
@@ -1,12 +1,14 @@
 package net.oxcodsnet.roadarchitect.fabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.oxcodsnet.roadarchitect.RoadArchitect;
 import net.oxcodsnet.roadarchitect.fabric.config.RAConfigFabricBridge;
 import net.oxcodsnet.roadarchitect.fabric.events.FabricEventBridge;
 import net.oxcodsnet.roadarchitect.fabric.events.RoadFeatureRegistryFabric;
 import net.oxcodsnet.roadarchitect.fabric.events.RoadGraphStateFabricEvents;
 import net.oxcodsnet.roadarchitect.fabric.events.RoadPipelineFabricEvents;
+import net.oxcodsnet.roadarchitect.handlers.compat.BopCompat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +18,7 @@ public final class RoadArchitectFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        BopCompat.setPresent(FabricLoader.getInstance().isModLoaded(BopCompat.MOD_ID));
         // 1) Поднять мост конфига (Cloth Config → common)
         RAConfigFabricBridge.bootstrap();
         RoadArchitect.init();

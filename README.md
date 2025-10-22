d84c048577825724bad5d77588a1450baadad89a
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2025-10-22 16:46:17 +0400

    v1.6.1 ---

--- README.md
+++ README.md
@@ -37,14 +37,12 @@
 
 # RoadArchitect
 
-**RoadArchitect** is a **Fabric / Quilt / NeoForge** mod for **Minecraft1.20.1, 1.21–1.21.8** that automatically scans the world for villages and other structures and connects them with roads to form a persistent travel network.  
+**RoadArchitect** is a **Fabric / Quilt / NeoForge / Forge ** mod for **Minecraft 1.20.1, 1.21–1.21.10** that automatically scans the world for villages and other structures and connects them with roads to form a persistent travel network.  
 Roads adapt their style to the biome, and the network is saved between game sessions.
 
-> **Current version:** `v1.6.0`
+> **Current version:** `v1.6.1`
 
 > **Wiki:** <a href="https://github.com/0xCoDSnet/RoadArchitect/wiki">RoadArchitect Wiki</a>
-
-> _For **Forge 1.20.1**, it works correctly through_ [**Sinytra Connector**](https://modrinth.com/mod/connector)
 
 ## ✨ Features
 
--- modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/client/RAKeybinds.java
+++ modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/client/RAKeybinds.java
@@ -2,27 +2,22 @@ package net.oxcodsnet.roadarchitect.neoforge.client;
 
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
-import net.oxcodsnet.roadarchitect.RoadArchitect;
 import org.lwjgl.glfw.GLFW;
 
-@EventBusSubscriber(
-        modid = RoadArchitect.MOD_ID,
-        bus = EventBusSubscriber.Bus.MOD,
-        value = Dist.CLIENT
-)
 public final class RAKeybinds {
     public static KeyBinding OPEN;
 
+    private RAKeybinds() {
+    }
 
-    @SubscribeEvent
-    public static void registerKeys(RegisterKeyMappingsEvent e) {
-        OPEN = new KeyBinding("key.roadarchitect.debug",
-                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H,
-                "category.roadarchitect");
-        e.register(OPEN);
+    public static void registerKeys(RegisterKeyMappingsEvent event) {
+        OPEN = new KeyBinding(
+                "key.roadarchitect.debug",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_H,
+                "category.roadarchitect"
+        );
+        event.register(OPEN);
     }
 }

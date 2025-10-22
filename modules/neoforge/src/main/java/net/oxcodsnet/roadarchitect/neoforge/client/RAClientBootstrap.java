2240789bb7b4e0799233add1536c5aca9204d46b
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2025-10-22 17:53:02 +0400

    v1.6.1 ---

--- modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/client/RAClientBootstrap.java
+++ modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/client/RAClientBootstrap.java
@@ -1,14 +1,23 @@
 package net.oxcodsnet.roadarchitect.neoforge.client;
 
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.ModLoadingContext;
 import net.oxcodsnet.roadarchitect.RoadArchitect;
+import net.oxcodsnet.roadarchitect.neoforge.client.hook.DebugGraphScreenHook;
+import net.oxcodsnet.roadarchitect.neoforge.client.hook.LoadingOverlaySubscriber;
 import net.oxcodsnet.roadarchitect.neoforge.config.RAConfigNeoForgeBridge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Proxy;
+import java.util.function.Supplier;
 
+/**
+ * Client-specific bootstrap for the NeoForge platform.
+ * Registers config screens, keybinds and render hooks when the dist is client.
+ */
 public final class RAClientBootstrap {
     private static final Logger LOGGER = LoggerFactory.getLogger(RoadArchitect.MOD_ID + "/" + RAClientBootstrap.class.getSimpleName());
     public static void init(ModContainer container) {

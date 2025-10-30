a1e30ba55a8695588d786a58dfbac19312bbb3fa
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2025-10-30 14:52:15 +0400

    Migration to mojmap [For full coverage of different platforms]

---
+++ modules/common/src/main/java/net/oxcodsnet/roadarchitect/config/RoadDecorationType.java
@@ -0,0 +1,26 @@
+package net.oxcodsnet.roadarchitect.config;
+
+import net.minecraft.util.StringRepresentable;
+
+/**
+ * Supported decoration kinds for configurable road styles.
+ */
+public enum RoadDecorationType implements StringRepresentable {
+    NONE("none"),
+    FENCE("fence");
+
+    private final String id;
+
+    RoadDecorationType(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getSerializedName() {
+        return this.id;
+    }
+
+    public String id() {
+        return this.id;
+    }
+}
